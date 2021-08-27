2b41df3ec0dc51ff759e0605b4bf992d75ea6733
Luke Burns <lpb325@lehigh.edu>
2021-08-27 16:25:51 -0400

    Added Information

--- Intro to R.R
+++ Intro to R.R
@@ -1,9 +1,9 @@
 # R script to accompany Intro to R
-# Copy for LukeBurns 2021
+# Copy for LukeBurns2021
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
 # in your script by putting "#" at the beginning of the lines.
-# 
+# Customized by LukeBurns2021
 # In fact, you can put comments anywhere on a line as long as 
 # you put "#" before them.
