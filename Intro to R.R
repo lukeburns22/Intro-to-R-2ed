e61ce568493ea9381ff9686e0cbda3434363ffeb
Luke Burns <lpb325@lehigh.edu>
2021-08-27 16:13:22 -0400

    Added Name (2)

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,5 @@
 # R script to accompany Intro to R
-# Copy for Luke Burns 2021
+# Copy for LukeBurns 2021
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
